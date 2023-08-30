4260f5d7c34bdbe4241388f51d7835cda54983e7
Tazoria <gayeonlee93@gmail.com>
2023-08-30 20:49:11 +0900

    토크나이저의 경로를 상대경로로 수정

--- models/utils/load_tokenizer.py
+++ models/utils/load_tokenizer.py
@@ -12,7 +12,7 @@ def load_tokenizer(vocab_path):
 
 
 if __name__ == '__main__':
-  vocab_path = r'D:\banimo_diary\models\vocab_32000.txt'
+  vocab_path = '../transformer/vocab_32000.txt'
   tokenizer = load_tokenizer(vocab_path)
   encoded = tokenizer.encode('안녕 나는 타조랑말코딱지야')
   decoded = tokenizer.decode(encoded)
